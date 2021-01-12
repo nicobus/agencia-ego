833d031e507adf0aa369907710edcb221bc786cb
Nicolas Bustamante <bustamante.n.f@gmail.com>
2021-01-12 01:41:14 -0300

    header

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div>
+        dfwfw
+    </div>
+</footer>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+
+@extends('layouts.layout')
+
+@section('title')
+    Agencia EGO | Home
+@endsection
+
+@section('content')
+<div>kfhkshfshg</div>
+@endsection
